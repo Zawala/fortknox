8168c5381dbca0de82b3eefc2805fd672e3212ee
Kelvin zawala <87924723+Zawala@users.noreply.github.com>
2026-03-23 07:17:59 +0200

    first commit

---
+++ src/main/java/fortknox/zw/ZwApplication.java
@@ -0,0 +1,13 @@
+package fortknox.zw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fortknox/zw/ZwApplicationTests.java
@@ -0,0 +1,13 @@
+package fortknox.zw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
